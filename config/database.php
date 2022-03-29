2eb6916a58e6770124a4aedc3f56c58f63c71796
Boney <boney11717102@gmail.com>
2022-03-29 17:52:16 +0100

    updated

--- config/database.php
+++ config/database.php
@@ -42,8 +42,8 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'frankHub'),
+            'port' => env('DB_PORT', '3307'),
+            'database' => env('DB_DATABASE', 'frankHub1'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
